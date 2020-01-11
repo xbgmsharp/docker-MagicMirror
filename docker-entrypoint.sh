06aea7039db3295a46595393fe9628c437f2db03
xbgmsharp <flacroix>
2020-01-11 05:18:54 -0500

    node_helper is now included in the default git clone install as part of the base so it is present as part of npm install

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,11 +5,6 @@ if [ ! -d /opt/magic_mirror/modules/default ]; then
     cp -r /opt/magic_mirror/default_modules/. /opt/magic_mirror/modules
 fi
 
-if [ ! -d /opt/magic_mirror/modules/node_modules/node_helper ]; then
-    echo "Copying node_helper dependencies"
-    cp -r /opt/magic_mirror/default_modules/node_modules /opt/magic_mirror/modules/
-fi
-
 if [ ! -d /opt/magic_mirror/config ]; then
     echo "Copying default config"
     cp -r /opt/magic_mirror/default_config/. /opt/magic_mirror/config
